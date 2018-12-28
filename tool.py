3d94ff0c21c843037a4fbd246fd511173d96ffe9
snahilindoria <snahilindoria999@gmail.com>
2018-12-29 02:47:25 +0530

    Commit #3

--- tool.py
+++ tool.py
@@ -1,5 +1,6 @@
 import os   # IMPORTING OS MODULE FOR 
 
+os.system("tput setaf 1")
 print (" \t\t\t  WELCOME TO THE REDHAT 7.5-7.0 AUTOMATION TOOL")
 print (" \t\t\t  ---------------------------------------------")
 
@@ -12,3 +13,11 @@ print (" \t\t\t  ---------------------------------------------")
 print ("Press 1 : For User Commands i.e (creation,deleation)")
 print ("Press 2 : For Checking Date")
 print ("Press 3 : For Cal") 
+
+
+print("Enter Your Choice")
+choice=input()
+
+    if int(choice)==1:
+       print("date")
+            
