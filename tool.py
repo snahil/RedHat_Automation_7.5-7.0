af6d8c25c9885c8ec51f34f7c75bc68a52ab3039
snahilindoria <snahilindoria999@gmail.com>
2018-12-30 01:16:01 +0530

    #5

--- tool.py
+++ tool.py
@@ -1,33 +1,50 @@
 import os
-import numpy
-import subprocess # IMPORTING OS MODULE FOR 
+import subprocess
+os.system("tput setaf 3")
+print ("\t\t\t WELCOME TO MY REDHAT AUTOMATION TOOL")
+os.system ("tput setaf 2")
+print ("\t\t\t ------------------------------------")
 
-os.system("tput setaf 1")
-print (" \t\t\t  WELCOME TO THE REDHAT 7.5-7.0 AUTOMATION TOOL")
-print (" \t\t\t  ---------------------------------------------")
+response = input("DO YOU WANT TO GET ACCESS remote OR local:    ")
+if (response == "local"):
+        print("\n\n Functionality Provided")
+        print("""
+        PRESS 1: To Check The Date
+        PRESS 2: To  Run Cal
+        PRESS 3: File Tool's i.e creating file/ deleating file
+        PRESS 4: User Tool's i.e adding a new user/ deleating a user
+        PRESS 5: 
+        PRESS 6:
+        PRESS 7:
+        PRESS 8:
+        PRESS 9:
+        PRESS 10:
+        PRESS 11: To Exit The Tool
+        """)
 
-
-
-
-
-
-
-print ("Press 1 : For User Commands i.e (creation,deleation)")
-print ("Press 2 : For Checking Date")
-print ("Press 3 : For Cal") 
-print ("Press 4 : Image Processing i.e image croping , pixel/Color_Modification")
-
-print("Enter Your Choice")
-choice=input()
-
-    if int(choice)==1:
-       print("date")
-       
-    if int(choice)==2:
-        
-    if int(choice)==3: 
-        
-    if int(choice)==4:
-        
-    else
-        print("WRONG SELECTION")
+        num=int(input())                
+        if(num==1):
+            x=subprocess.getoutput("date")
+            print(x)
+        elif(num==2):
+            x=subprocess.getoutput("cal")
+            print(x)   
+        elif(num==3):
+            print("""
+            PRESS 1: TO ADD A NEW FILE   
+            PRESS 2: TO REMOVE A FILE
+            """)
+        choice=int(input())
+        if(choice==1):
+                  f_name=input("Enter the file you wish to create")   
+                  f_location=input("Enter the location of file where you want to place it")
+                  handle=subprocess.getstatusoutput("touch {}/{}".format(f_location,f_name))
+        if(handle[0]==0):
+                      print("File Created Sucessfully @!!!!!") 
+                      res = input("Open the file? Y/N")
+                      if(res=="yes" or res=="y"):
+                          os.system("gedit %s.py"%f_name)
+        else
+            print("Failed to create file. Description: {}".format(handle[1]))
+            os.system("tput setaf 4")                  
+            
