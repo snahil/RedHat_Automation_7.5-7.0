74cda326f6c475a9f54eba9fbda692ba562e2b3f
snahilindoria <snahilindoria999@gmail.com>
2019-01-18 12:57:24 +0530

    commit

--- index.py
+++ index.py
@@ -3,3 +3,26 @@ import os
 import requests
 import numpy as np
 import cv2
+
+
+print("WELCOME TO MY TOOL FOR REDHAT AUTOMATION")
+print("========================================")
+print ("USING THE TOOL LOCALY OR REMOTELY")
+ch=input()
+
+    
+
+
+
+
+
+
+print("""
+    PRESS 1 FOR HADOOP CLUSTER FORMATION
+    PRESS 2 FOR LIVE STREAMING FROM MOBILE
+    PRESS 3 FOR FACE RECOGIANTION
+      """)
+
+
+
+
