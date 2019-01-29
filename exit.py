dd28027a26c77c5cdbcf30f05939f8be90cf4eaf
snahilindoria <snahilindoria999@gmail.com>
2019-01-29 16:26:25 +0530

    commit

---
+++ exit.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 29 16:24:49 2019
+
+@author: Pablo
+"""
+
+import os
+exit()
--- index.py
+++ index.py
@@ -1,28 +1,36 @@
-import pandas
+#import pandas
 import os
-import requests
-import numpy as np
-import cv2
+#import requests
+#import numpy as np
+#import cv2
 
 
 print("WELCOME TO MY TOOL FOR REDHAT AUTOMATION")
 print("========================================")
 print ("USING THE TOOL LOCALY OR REMOTELY")
 ch=input()
-
-    
-
-
-
-
-
-
+if ch == 'localy'
 print("""
     PRESS 1 FOR HADOOP CLUSTER FORMATION
     PRESS 2 FOR LIVE STREAMING FROM MOBILE
     PRESS 3 FOR FACE RECOGIANTION
       """)
 
+ x = raw_input("Enter Your Choice:     ")
+ 
+ if x == '0'
+ 
+     os.system('hadoopcluster.py')
+     
+     
+  if x == '1'
+      
+  os.system(hadoop.py)
+      
+
+
+ if x == "10"   
+     os.system('exit.py')
 
 
 
