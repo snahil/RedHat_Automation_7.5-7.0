5b69703746f4cbb75479f33dbf65591db179f8a2
snahilindoria <snahilindoria999@gmail.com>
2019-02-12 13:42:43 +0530

    commit

--- software.py
+++ software.py
@@ -1,1 +1,27 @@
 #!/bin/usr/python36
+
+
+#import pandas
+import os
+#import requests
+#import numpy as np
+#import cv2
+
+
+print("WELCOME TO MY TOOL FOR REDHAT AUTOMATION")
+print("========================================")
+print ("USING THE TOOL LOCALY OR REMOTELY")
+ch=input()
+if ch == 'localy'
+print("""
+    PRESS 0 FOR HADOOP CLUSTER FORMATION
+    press 1 FOR SOFTWARE USE AND FUNCTIONALITY
+    PRESS 2 FOR LIVE STREAMING FROM MOBILE
+    PRESS 3 FOR FACE RECOGIANTION
+    PRESS 10 FOR EXIT THE TOOL
+      """)
+
+ x = raw_input("Enter Your Choice:     ")
+ 
+while true:
+#!/bin/usr/python36
