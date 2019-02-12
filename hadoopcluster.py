77ce46454a1a554d0ebe839e82f1f70b08365987
snahilindoria <snahilindoria999@gmail.com>
2019-02-12 13:40:51 +0530

    commit

--- hadoopcluster.py
+++ hadoopcluster.py
@@ -1,7 +1,26 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan 30 12:05:00 2019
+#!/bin/usr/python36
 
-@author: Pablo
-"""
 
+#import pandas
+import os
+#import requests
+#import numpy as np
+#import cv2
+
+
+print("WELCOME TO MY TOOL FOR REDHAT AUTOMATION")
+print("========================================")
+print ("USING THE TOOL LOCALY OR REMOTELY")
+ch=input()
+if ch == 'localy'
+print("""
+    PRESS 0 FOR HADOOP CLUSTER FORMATION
+    press 1 FOR SOFTWARE USE AND FUNCTIONALITY
+    PRESS 2 FOR LIVE STREAMING FROM MOBILE
+    PRESS 3 FOR FACE RECOGIANTION
+    PRESS 10 FOR EXIT THE TOOL
+      """)
+
+ x = raw_input("Enter Your Choice:     ")
+ 
+while true:
