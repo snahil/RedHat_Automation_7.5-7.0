9270e0f4c72baf658d2e525f5f6389e9a7fcb5f4
snahilindoria <snahilindoria999@gmail.com>
2019-02-12 15:18:31 +0530

    commit

--- software.py
+++ software.py
@@ -20,8 +20,29 @@ print("""
     PRESS 3 FOR FACE RECOGIANTION
     PRESS 10 FOR EXIT THE TOOL
       """)
+if ch == '1'
+print (""" 
+       PRESS A FOR INSTALLING A SOFTWARE PACKAGE WITH RPM PACKAGE MANAGER.
+       PRESS B FOR INSTALLING A SOFTWARE PACKAGE WITH YUM.
+       PRESS C FOR INSTALLING A SOFTWARE PACKAGE USING PIP-2 FROM THE WEB.
+       PRESS D FOR INSTALLING A SOFTWARE PACKAGE USING PIP-3 FROM THE WEB.
+       PRESS E FOR UNINSTALL A SOFTWARE PACKAGE FROM THE SYSTEM.
+       PRESS F FOR  KNOWLEDGE BASE.
+       PRESS X FOR RETURNING TO THE MAIN MENU.
+       PRESS Y FOR EXIT THE TOOL.
+        """)
 
- x = raw_input("Enter Your Choice:     ")
- 
-while true:
-#!/bin/usr/python36
+
+x = raw_input ("Enter Your Choice")
+
+
+
+if ch == 'A'
+            m = raw_input("Enter The name of the tool which you want to install  :         ") 
+            n = raw_input("Enter the path of the tool i.e /root/Desktop/hadoop.rpm")
+            os.system ( 'rpm -ivh' + m)  
+if ch == 'B' 
+            m =raw_input 
+
+
+
